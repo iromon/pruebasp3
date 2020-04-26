b7c374b7da59972f4a124ea2b40ca5bec121e43f
isabelromon <i.romon@alumnos.upm.es>
2020-04-26 15:39:03 +0200

    pruebas

--- src/main/java/etsisi/ems2020/trabajo3/lineadehorizonte/Ciudad.java
+++ src/main/java/etsisi/ems2020/trabajo3/lineadehorizonte/Ciudad.java
@@ -101,16 +101,15 @@ ciudad = new ArrayList <Edificio>();
      * edificio solapa a otro, si hay edificios contiguos, etc. y solucionar dichos
      * problemas para que el LineaHorizonte calculado sea el correcto.
      */
-  /*  
-   public void hayElementos(LineaHorizonte s1,LineaHorizonte s2, LineaHorizonte salida) {
+     public void hayElementos(LineaHorizonte s1, LineaHorizonte s2, LineaHorizonte salida) {
     	
     	Punto p1 = new Punto();         // punto donde guardaremos el primer punto del LineaHorizonte s1
         Punto p2 = new Punto();         // punto donde guardaremos el primer punto del LineaHorizonte s2
-
-    	
-    	p1 = s1.getPunto(0); // guardamos el primer elemento de s1
+        
+        p1 = s1.getPunto(0); // guardamos el primer elemento de s1
         p2 = s2.getPunto(0); // guardamos el primer elemento de s2
-
+        
+        
         if (p1.getX() < p2.getX()) // si X del s1 es menor que la X del s2
         {
             utilizarPrimerHorizonte(p1,salida);
@@ -128,16 +127,12 @@ ciudad = new ArrayList <Edificio>();
             s2.borrarPunto(0);
         }
     }
-    */
     
-   public LineaHorizonte LineaHorizonteFussion(LineaHorizonte s1,LineaHorizonte s2)
+    
+    public LineaHorizonte LineaHorizonteFussion(LineaHorizonte s1,LineaHorizonte s2)
     {    	
         LineaHorizonte salida = new LineaHorizonte(); // LineaHorizonte de salida
-        
-        
-        Punto p1 = new Punto();         // punto donde guardaremos el primer punto del LineaHorizonte s1
-        Punto p2 = new Punto();         // punto donde guardaremos el primer punto del LineaHorizonte s2
-        
+    
         inicializarVariables();
         imprimirBanner(s1,s2);
 
@@ -146,31 +141,14 @@ ciudad = new ArrayList <Edificio>();
         {	
         	//hayElementos(s1,s2,salida);
         	
-            p1 = s1.getPunto(0); // guardamos el primer elemento de s1
-            p2 = s2.getPunto(0); // guardamos el primer elemento de s2
+            
+            hayElementos(s1,s2,salida);
 
-            if (p1.getX() < p2.getX()) // si X del s1 es menor que la X del s2
-            {
-            	utilizarPrimerHorizonte(p1,salida);
-                s1.borrarPunto(0); // en cualquier caso eliminamos el punto de la linea horizonte
-            }
-            else if (p1.getX() > p2.getX()) // si X del s1 es mayor que la X del s2
-            {
-            	utilizarSegundoHorizonte(p2,salida);
-                s2.borrarPunto(0);
-            }
-            else // si la X del s1 es igual a la X del s2
-            {
-                utilizarHorizonteMasAlto(p1,p2,salida);
-                s1.borrarPunto(0); // eliminamos el punto del s1 y del s2
-                s2.borrarPunto(0);
-            }
         }
         utilizarUnaLineaHorizonte(s1,salida);
         utilizarUnaLineaHorizonte(s2,salida);
         return salida;
-    }
-    
+    }    
     private void inicializarVariables() {
     	this.alturaAnteriorPuntoUno = -1;
     	this.alturaAnteriorPuntoDos = -1;
